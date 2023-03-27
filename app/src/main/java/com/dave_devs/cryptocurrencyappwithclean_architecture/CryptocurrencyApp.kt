a3d8b2b1c9b0166bae4bd3d3148b290fbd8efc3e
Oreoluwa Odetunde <102271576+LSR07@users.noreply.github.com>
2023-03-27 02:15:44 +0100

    First commit 0.6

--- app/src/main/java/com/dave_devs/cryptocurrencyappwithclean_architecture/CryptocurrencyApp.kt
+++ app/src/main/java/com/dave_devs/cryptocurrencyappwithclean_architecture/CryptocurrencyApp.kt
@@ -4,4 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class CryptocurrencyApp: Application()
+class CryptocurrencyApp: Application() {}
--- app/src/main/java/com/dave_devs/cryptocurrencyappwithclean_architecture/feature_cryptocureency/di/AppModule.kt
+++ app/src/main/java/com/dave_devs/cryptocurrencyappwithclean_architecture/feature_cryptocureency/di/AppModule.kt
@@ -1,9 +1,11 @@
-package com.dave_devs.cryptocurrencyappwithclean_architecture.di
+package com.dave_devs.cryptocurrencyappwithclean_architecture.feature_cryptocureency.di
 
 import com.dave_devs.cryptocurrencyappwithclean_architecture.common.Constant.BASE_URL
 import com.dave_devs.cryptocurrencyappwithclean_architecture.feature_cryptocureency.data.remote.CoinPaprikaApi
 import com.dave_devs.cryptocurrencyappwithclean_architecture.feature_cryptocureency.data.remote.repository.CoinRepositoryImpl
 import com.dave_devs.cryptocurrencyappwithclean_architecture.feature_cryptocureency.domain.repository.CoinRepository
+import com.dave_devs.cryptocurrencyappwithclean_architecture.feature_cryptocureency.domain.use_cases.GetCoinUseCase
+import com.dave_devs.cryptocurrencyappwithclean_architecture.feature_cryptocureency.domain.use_cases.GetCoinsUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -30,5 +32,17 @@ object AppModule {
     @Singleton
     fun provideCryptoRepository(api: CoinPaprikaApi): CoinRepository {
         return CoinRepositoryImpl(api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetCoinsUseCase(repository: CoinRepository): GetCoinsUseCase {
+        return GetCoinsUseCase(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetCoinUseCase(repository: CoinRepository): GetCoinUseCase {
+        return GetCoinUseCase(repository)
     }
 }
